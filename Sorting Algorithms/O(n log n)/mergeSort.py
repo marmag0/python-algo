6dc259951a4c6238d6a00c7109ec51e99346bf45
Mikołaj Mazur <mikolajmazur@Mikoajs-MacBook-Pro.local>
2025-07-02 16:30:26 +0200

    folder name change

---
+++ Sorting Algorithms/O(n log n)/mergeSort.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Recursively dividing the list into smaller sublists until each sublist has one element, 
+# then merging the sublists back together in sorted order.
+
+def selectionSort(arr, l):
+    pass
+
+def main():
+    array = [1, 4, 5, 3, 9, 4]
+    length = len(array)
+
+    selectionSort(array, length)
+    print(array)
+
+if __name__ == "__main__":
+    main()
